3a63d9a81f6f0cad204b7e3a70e6a7d72462b21b
Arthur Helfstein Fragoso <arthur@life.net.br>
2015-01-20 05:36:18 -0200

    Updated Laravel 4.0 to 4.2

--- app/models/User.php
+++ app/models/User.php
@@ -34,7 +34,20 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		});
     }
 
-	
+	public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
 
 	/**
 	 * Get the unique identifier for the user.
